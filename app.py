57f5c3c5ccdc48f0d4a4105644a739250b57f7b9
Bijin Nhuchhe Pradhan <bijinpradhan@gmail.com>
2021-06-20 11:04:46 +0545

    Background threads run, url still hardcoded

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import cv2
 from bisenetv1 import BiSeNetV1
-from utils import pre_process, post_process, direct, get_prediction, prediction_thread
+from utils import pre_process, post_process, direct, get_prediction
 from flask import Flask, jsonify, request
 from threading import Thread
 
@@ -13,10 +13,37 @@ model.load_state_dict(torch.load('./res/model_final_v1.pth', map_location=torch.
 model.cuda()
 model.eval()
 
-last = np.array([320, 320, 320]).astype(np.float32)
+last = np.array([320, 320, 320, 320]).astype(np.float32)
 last = torch.from_numpy(last).cuda()
-url = ""
 
+frame = 0.0
+url = r"D:\code\Python\NN\hackbout\roadVideo_Trim.mp4"
+vs = cv2.VideoCapture(url)
+fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+writer = cv2.VideoWriter("out.avi", fourcc, 30, 
+                    (640, 320), True)
+
+
+def prediction_thread(model):
+    global last
+    global frame
+    while True:
+        grabbed, img = vs.read()
+        if not grabbed:
+            break
+        img = cv2.resize(img, (640, 320))
+        input_tensor = pre_process(img)
+        out_tensor = get_prediction(model, input_tensor)
+        img, last = post_process(img, out_tensor, last) 
+        frame +=1
+        writer.write(img)
+    # return last
+    writer.release()
+    vs.release()
+
+
+thread = Thread(target=prediction_thread, args=(model,), daemon=True)
+thread.start()
 
 @app.route('/', methods=['GET'])
 def root():
@@ -24,24 +51,14 @@ def root():
 
 
 @app.route('/predict', methods=['POST'])
-def predict()):
+def predict():
+    global frame
+    global last
     if request.method == 'POST':
-        vs = cv2.VideoCapture(url)
-        fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-        writer = cv2.VideoWriter("out.avi", fourcc, 30, 
-                            (640, 320), True)
-        if url is not None:
-            _, img = vs.read() 
-            input_tensor = pre_process(img)
-            out_tensor = get_prediction(model, input_tensor)
-            img, last = post_process(img, out_tensor, last)
-
-            return jsonify({'class_id': class_id, 'class_name': class_name})
-        return jsonify({"url": url})
+        # last = prediction_thread(model, last, offset)
+        ret = last[0].cpu().numpy()
+        return jsonify({"pos": int(ret), "frame": frame})
 
 
 if __name__ == '__main__':
-    thread = Thread(target=prediction_thread, args=(url, model, last))
-    thread.daemon = True
-    thread.start()
     app.run()
--- utils.py
+++ utils.py
@@ -12,49 +12,45 @@ COLORS = open("./res/enet-colors.txt").read().strip().split("\n")
 COLORS = [np.array(c.split(",")).astype("int") for c in COLORS]
 COLORS = np.array(COLORS, dtype="uint8")
 COLORS = torch.from_numpy(COLORS).cuda()
+MEAN=(0.3257, 0.3690, 0.3223) # city, rgb
+STD=(0.2112, 0.2148, 0.2115)
+
 
 
 def pre_process(img):
-    img = img.transpose(2, 0, 1).astype(np.float32)
-    img = torch.from_numpy(img).div_(255)
-    dtype, device = img.dtype, img.device
+    input_tensor = img.transpose(2, 0, 1).astype(np.float32)
+    input_tensor = torch.from_numpy(input_tensor).div_(255).cuda()
+    dtype, device = input_tensor.dtype, input_tensor.device
     mean = torch.as_tensor(MEAN, dtype=dtype, device=device)[:, None, None]
     std = torch.as_tensor(STD, dtype=dtype, device=device)[:, None, None]
-    img = img.sub_(mean).div_(std).clone()
-    return img_tensor
+    input_tensor = input_tensor.sub_(mean).div_(std).clone()
+    return input_tensor.unsqueeze(0)
 
 
 def direct(out_tensor, last):
     zeros = torch.zeros(640).cuda()
     out_tensor = out_tensor.sum(dim=2)
-    out_tensor = 320 - out_tensor.count_nonzero(dim=0)
+    out_tensor = 320 - torch.count_nonzero(out_tensor, dim=0)
     m = out_tensor.argmax()
     last = last / 2 + 160
-    m = (zeros.argmax() + last.mean()) / 2 
-    last[:3] = last[1:].clone()
-    last[3] = m
-    return m, last
+    m = (m + last.mean()) / 2 
+    new_last = last.clone()
+    new_last[:3] = last[1:]
+    new_last[-1] = m
+    return m, new_last
 
 
 def post_process(img, out_tensor, last):
     m, last = direct(out_tensor, last)
-    m = m[0].cpu().numpy()
+    m = int(m.cpu().item())
     img[296:320, m-4:m+4] = mark
     img[292:296, :] = bar
     return img, last
 
 
-def get_prediction(model, img):
-    input_tensor = pre_process(img)
+def get_prediction(model, input_tensor):
     out_tensor = model(input_tensor)[0].argmax(dim=1).squeeze().detach()
     out_tensor = COLORS[out_tensor]
     return out_tensor
 
 
-def prediction_thread(url, model, last):
-    vs = cv2.VideoCapture(url)
-    grabbed = True
-    while grabbed:
-        grabbed, img = vs.read()
-        out_tensor = get_prediction(model, img)
-        img, last = post_process(img, out_tensor, last) 
